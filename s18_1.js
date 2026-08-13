a19a49ebc57bd048cb02dcb0a3658aa8466ddee1
level262728-dev <level262728@yahoo.co.jp>
2026-08-13 11:53:25 +0900

    Add files via upload

---
+++ s18_1.js
@@ -0,0 +1,14 @@
+console.log("--- BÀI 1 ---");
+
+const person = {
+  name: "Nguyễn Văn B",
+  age: 25,
+  address: "Hà Nội",
+  phone: "0987654321"
+};
+
+// Truy xuất các thuộc tính của đối tượng bằng Dot Notation (.)
+console.log("Tên:", person.name);
+console.log("Tuổi:", person.age);
+console.log("Địa chỉ:", person.address);
+console.log("Số điện thoại:", person.phone);
